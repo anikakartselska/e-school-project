86fee08ec64db9bc402d22067f7d2b7f2dcb90ea
Anika <anika1931@abv.bg>
2024-05-26 16:47:15 +0300

    school lessons page

--- frontend/src/pages/administration/components/school-program-component-per-semester.vue
+++ frontend/src/pages/administration/components/school-program-component-per-semester.vue
@@ -4,61 +4,114 @@
             програма
             за срока
         </div>
+        <q-btn v-if="!hasLessons && currentUserHasAnyRole([SchoolRole.ADMIN])" class="q-ml-md text-primary"
+               label="Генетирай програмите"
+               outline
+               @click="generateProgram()"
+        ></q-btn>
     </div>
     <div v-else>
-        <div class="row">
-            <q-select v-model="selectedSchoolClass"
-                      :option-label="option => option.name"
-                      :options="schoolClasses"
-                      class="q-pl-md"
-                      dense
-                      label="Клас"
-                      reactive-rules
-                      style="width: 250px"/>
-            <q-space/>
-            <q-btn v-if="!hasLessons && currentUserHasAnyRole([SchoolRole.ADMIN])" class="q-mr-md text-negative"
-                   label="Генетирай програмите за всички класове отново"
+        <div v-if="!hasLessons && currentUserHasAnyRole([SchoolRole.ADMIN])" class="row q-mb-md q-ml-md">
+            <q-btn class="q-mr-md text-negative"
+                   label="Генетирай програмите отново"
                    outline
                    @click="generateProgram()"
             ></q-btn>
-            <q-btn v-if="!hasLessons && currentUserHasAnyRole([SchoolRole.ADMIN])" class="q-mr-md text-secondary"
+            <q-btn class="q-mr-md text-secondary"
                    label="Генерирай седмични разписи"
                    outline
                    @click="generateSchoolLessonsForAllClasses()"
             ></q-btn>
         </div>
-        <q-separator class="q-mt-md q-mb-md"/>
-        <div class="row">
-            <div v-for="day in Object.keys(WorkingDays)" class="col-2 q-ma-sm">
-                <div class=" text-center text-primary text-h6">
-                    {{ workingDaysTranslation.get(day) }}
-                </div>
-                <q-card v-for="lesson in plannedLessonsGroupedByDay.get(WorkingDays[day])" class="q-mb-sm"
-                        style="height: 15vh">
-                    <q-card-section>
-                        Предмет:<span class="text-primary">
-              {{
-                        lesson.subject
-                        }}</span><br>
-                        Учител:<span class="text-primary">
-              {{ lesson.teacher.firstName }} {{ lesson.teacher.lastName }}</span>
-                        <br>
-                        Стая:<span class="text-primary">
-              {{
-                        lesson.room.room
-                        }}
-                  </span>
-                    </q-card-section>
-                </q-card>
+        <q-separator/>
+        <q-tabs v-model="selectedTab" dense>
+            <q-tab :label="`За класове`"
+                   name="FOR_CLASS"/>
+            <q-tab label="За учители" name="FOR_TEACHER"/>
+        </q-tabs>
+        <div v-if="selectedTab === 'FOR_CLASS'">
+            <div class="row">
+                <q-select v-model="selectedSchoolClass"
+                          :option-label="option => option.name"
+                          :options="schoolClasses"
+                          class="q-pl-md"
+                          dense
+                          label="Клас"
+                          reactive-rules
+                          style="width: 250px"/>
             </div>
+            <q-separator class="q-mt-md q-mb-md"/>
+            <div class="row">
+                <div v-for="day in Object.keys(WorkingDays)" class="col-2 q-ma-sm">
+                    <div class=" text-center text-primary text-h6">
+                        {{ workingDaysTranslation.get(day) }}
+                    </div>
+                    <q-card v-for="lesson in plannedLessonsGroupedByDay.get(WorkingDays[day])" class="q-mb-sm"
+                            style="height: 15vh">
+                        <q-card-section>
+                            Предмет:<span class="text-primary">
+              {{
+                            lesson.subject
+                            }}</span><br>
+                            Учител:<span class="text-primary">
+              {{ lesson.teacher.firstName }} {{ lesson.teacher.lastName }}</span>
+                            <br>
+                            Стая:<span class="text-primary">
+              {{
+                            lesson.room.room
+                            }}
+                  </span>
+                        </q-card-section>
+                    </q-card>
+                </div>
+            </div>
+            <div v-if="!selectedSchoolClass" class="q-pl-md text-negative">Изберете клас, за да видите програмата</div>
         </div>
-        <div v-if="!selectedSchoolClass" class="q-pl-md text-negative">Изберете, клас, за да видите програмата</div>
+        <div v-if="selectedTab === 'FOR_TEACHER'">
+            <div class="row">
+                <q-select v-model="selectedTeacher"
+                          :option-label="option => `${option.firstName} ${option.lastName}`"
+                          :options="teachers"
+                          class="q-pl-md"
+                          dense
+                          label="Учител"
+                          reactive-rules
+                          style="width: 250px"/>
+            </div>
+            <q-separator class="q-mt-md q-mb-md"/>
+            <div class="row">
+                <div v-for="day in Object.keys(WorkingDays)" class="col-2 q-ma-sm">
+                    <div class=" text-center text-primary text-h6">
+                        {{ workingDaysTranslation.get(day) }}
+                    </div>
+                    <q-card v-for="lesson in plannedLessonsGroupedByDay.get(WorkingDays[day])" class="q-mb-sm"
+                            style="height: 15vh">
+                        <q-card-section>
+                            Предмет:<span class="text-primary">
+              {{
+                            lesson.subject
+                            }}</span><br>
+                            Клас:<span class="text-primary">
+                {{ lesson.schoolClass.name }}</span>
+                            <br>
+                            Стая:<span class="text-primary">
+              {{
+                            lesson.room.room
+                            }}
+                  </span>
+                        </q-card-section>
+                    </q-card>
+                </div>
+            </div>
+            <div v-if="!selectedTeacher" class="q-pl-md text-negative">Изберете учител, за да видите програмата</div>
+        </div>
     </div>
 </template>
 <script lang="ts" setup>
 import {
     checkExistingPlannedSchoolLessonsForSemester,
     checkExistingSchoolLessonsForSemester,
+    fetchAllTeachers,
     fetchPlannedSchoolLessonsForSchool,
     generatePlannedSchoolLessonsForSchool,
     generateSchoolLessons,
@@ -75,7 +128,7 @@ import {
 import {watch} from "vue";
 import {$ref} from "vue/macros";
 import {currentUserHasAnyRole} from "../../../services/LocalStorageService";
-import {SchoolRole} from "../../../model/User";
+import {SchoolRole, TeacherView} from "../../../model/User";
 import {confirmActionPromiseDialog} from "../../../utils";
 
 const props = defineProps<{
@@ -89,6 +142,9 @@ let selectedSchoolClass = $ref<SchoolClass | null>(null)
 const plannedLessonsGroupedByDay = $ref(new Map<WorkingDays, PlannedSchoolLesson[]>())
 let hasPlannedLessons = $ref(await checkExistingPlannedSchoolLessonsForSemester(props.schoolId, props.periodId, props.semester))
 let hasLessons = $ref(await checkExistingSchoolLessonsForSemester(props.schoolId, props.periodId, props.semester))
+const teachers = await fetchAllTeachers(props.schoolId, props.periodId)
+let selectedTeacher = $ref<TeacherView | null>(null)
+let selectedTab = $ref("FOR_CLASS")
 const generateProgram = async () => {
     await confirmActionPromiseDialog("Сигурни ли сте, че искате да продължите?")
     await generatePlannedSchoolLessonsForSchool(props.schoolId, props.periodId, props.semester).then(
@@ -108,13 +164,20 @@ const generateSchoolLessonsForAllClasses = async () => {
     )
 }
 let plannedLessons = $ref<PlannedSchoolLesson[]>([])
-watch(() => selectedSchoolClass, () => {
-            if (selectedSchoolClass == null) {
+watch(() => [selectedSchoolClass, selectedTeacher, selectedTab], () => {
+            if ((selectedTab === 'FOR_CLASS' && selectedSchoolClass == null) || (selectedTab === 'FOR_TEACHER' && selectedTeacher == null)) {
                 plannedLessons = []
+                plannedLessonsGroupedByDay.clear()
             } else {
-                plannedLessons = schoolPlannedLessons.filter(it => it.schoolClass.id == selectedSchoolClass!!.id).sort(function (a, b) {
-                    return workingDaysOrder.get(a.workingHour.workingDays)!! - workingDaysOrder.get(b.workingHour.workingDays)!! || a.workingHour.hour - b.workingHour.hour;
-                })
+                if (selectedTab === 'FOR_CLASS') {
+                    plannedLessons = schoolPlannedLessons.filter(it => it.schoolClass.id == selectedSchoolClass!!.id).sort(function (a, b) {
+                        return workingDaysOrder.get(a.workingHour.workingDays)!! - workingDaysOrder.get(b.workingHour.workingDays)!! || a.workingHour.hour - b.workingHour.hour;
+                    })
+                } else {
+                    plannedLessons = schoolPlannedLessons.filter(it => it.teacher.id == selectedTeacher!!.id).sort(function (a, b) {
+                        return workingDaysOrder.get(a.workingHour.workingDays)!! - workingDaysOrder.get(b.workingHour.workingDays)!! || a.workingHour.hour - b.workingHour.hour;
+                    })
+                }
                 Object.keys(WorkingDays).forEach(day => {
                     plannedLessonsGroupedByDay.set(day, plannedLessons.filter(lesson => lesson.workingHour.workingDays == day))
 
